12e72701a2488a76cfd7297893bfda21fcd7ccb7
2019073309 <b379514@hanyang.ac.kr>
2020-08-22 23:01:24 +0900

    coding

--- app/src/main/java/com/example/study01/MainActivity.kt
+++ app/src/main/java/com/example/study01/MainActivity.kt
@@ -1,5 +1,7 @@
 package com.example.study01
 
+import android.content.Intent
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import kotlinx.android.synthetic.main.activity_main.*
@@ -9,8 +11,21 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        button.setOnClickListener {
-            textView.text="Button Clicked"
+
+        sunny.setOnClickListener {
+            textView.text="맑은 날"
+            imageView.setImageResource(R.drawable.sunny02)
+        }
+
+        cloudy.setOnClickListener {
+            textView.text="흐린 날"
+            imageView.setImageResource(R.drawable.cloudy01)
+        }
+
+        rain.setOnClickListener {
+            textView.text="비 오는 날"
+            imageView.setImageResource(R.drawable.rainy02)
         }
     }
+
 }
